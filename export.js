405eb3cc02b69bc29cbe3f45adb38158b6832dc3
Yosep Rivaldo Silaban <rivaldo.yoseps@gmail.com>
2022-08-13 15:23:05 +0700

    Perkenalan NPM, create project, run project, script, export module

---
+++ export.js
@@ -0,0 +1,4 @@
+import { writeToFile } from "nodejs-npm/write.js";
+
+
+writeToFile("export.log", "Belajar Export")
---
+++ index.js
@@ -0,0 +1,5 @@
+import { writeToFile } from "./write.js";
+
+writeToFile("hello.log", "Yosep R Silaban")
+
+console.info("Hello World")
